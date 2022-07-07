5b964052a4c2d460da9c37577a40b8dfcd2ac4a8
efcruz <eduarda.web.dev@gmail.com>
2022-07-07 12:26:51 +0200

    giftedchat implementation

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,38 @@
 import React from 'react';
 import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: "Hello developer",
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: "React Native",
+                        avatar: "https://placeimg.com/140/140/any",
+                    },
+                },
+            ],
+        });
+    }
+
+    //function called when sending a message
+    onSend(messages = []) {
+        this.setState((previousState) => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
+    }
 
     render() {
         let name = this.props.route.params.name;
@@ -9,13 +40,22 @@ export default class Chat extends React.Component {
         const { backgroundColor } = this.props.route.params;
 
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: backgroundColor }}>
-                <Text>Hello Chat!</Text>
+            <View style={{ flex: 1, backgroundColor: backgroundColor }}>
+
+                {/*  
                 <Button
                     title="Go to Start"
                     onPress={() => this.props.navigation.navigate('Start')}
+                /> */}
+                <GiftedChat
+                    messages={this.state.messages}
+                    onSend={(messages) => this.onSend(messages)}
+                    user={{
+                        _id: 1,
+                    }}
                 />
             </View>
         )
     }
+
 }
